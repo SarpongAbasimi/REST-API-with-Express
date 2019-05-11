b9c4e8b49c147f060ff85d57760aba83be47405c
sarpong <abasimisarpong@rocketmail.com>
2019-05-12 00:12:59 +0100

    Added express validators to validate user input

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express')
  ,app = express()
  ,path = require('path')
  ,mongoose = require('mongoose')
- ,bodyParser = require('body-parser');
+ ,bodyParser = require('body-parser')
+ ,expressValidator = require('express-validator');
 
 require('dotenv').config();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,('views')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(require('./routes/index'));
 app.use('/homes', require('./routes/home'));
 app.use('/registration', require('./routes/registration'));
 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,('views')));
 
-const uri =process.env.MONGODB_URL;
+
+
+const uri = process.env.MONGODB_URL;
 mongoose.connect(uri, { useNewUrlParser: true });
 
 let port = process.env.PORT || 3000
--- controllers/registration.js
+++ controllers/registration.js
@@ -9,24 +9,26 @@ exports.registration = (req, res)=> {
 
 
 exports.submitRegistration = (req, res)=> {
-  Bcrypt.hash(req.body.password, 10,(err, hash)=>{
-    if(err){
-      console.log(err.message)
-    }
-    const { name, email } = req.body
-    const user = new User({
-      _id: mongoose.Types.ObjectId(),
-      name,
-      email,
-      password: hash
+  req.check('email','The email you entered is invalid').isEmail();
+  req.check('password','Password must be at least 6 characters long').isLength({ min: 3});
+  req.check('password','Password must be equal to your confirmation password').equals(req.body.confirm_password);
+
+  const errors = req.validationErrors();
+  if(errors){
+    res.render('signup', {errors:errors})
+  }else{
+    const {email, name, password} = req.body
+    Bcrypt.hash(password, 10,(err, hash)=>{
+      if(err){ console.log('There was an error')}
+      const user = new User({_id: mongoose.Types.ObjectId(), name, email, password: hash});
+      user.save().then(result =>{ console.log('registration was successful')})
+      .catch(err=> console.log('There was an error'))
     });
-    user.save().then(result =>{ console.log('registration was successful')})
-    .catch(err=> console.log('There was an error'))
-  });
-  res.redirect('/registration/login')
+    res.redirect('/registration/login')
+  };
 };
 
 exports.login = (req, res)=>{
-  res.render('login');
+  res.send('hey')
 }
 
