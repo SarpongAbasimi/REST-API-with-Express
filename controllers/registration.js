9e9546a89f6a3761850db86c0eac4a563f8657b9
sarpong <abasimisarpong@rocketmail.com>
2019-05-14 21:56:44 +0100

    Refactored registration controller to output message to the web if an email is already taken when a user is registering

--- controllers/registration.js
+++ controllers/registration.js
@@ -2,11 +2,8 @@ const User = require('../model/User')
 ,mongoose = require('mongoose')
 ,Bcrypt = require('bcryptjs');
 
-
 exports.registration = (req, res)=> {
   const errors = req.session.errors
-  const duplicateEmail = req.session.duplicateEmail
-  console.log(duplicateEmail )
   res.render('signup',{errors:errors})
 };
 
@@ -15,26 +12,27 @@ exports.submitRegistration = (req, res)=> {
   req.check('password','Password must be at least 6 characters long').isLength({ min: 6});
   req.check('password','Password must be equal to your confirmation password').equals(req.body.confirm_password);
 
-  const errors = req.validationErrors();
-  if(errors){
-    req.session.errors = errors
-    res.redirect('/registration/signup')
-  }else{
-    const {email, name, password} = req.body
-    User.findOne({email}, (err,emailFound)=>{
-      if(err){
-        req.session.duplicateEmail = ['Sorry, that email is already taken']
-        res.redirect('/registration/signup')
-      }
-    });
-    Bcrypt.hash(password, 10,(err, hash)=>{
-      if(err){ console.log('There was an error')}
-      const user = new User({_id: mongoose.Types.ObjectId(), name, email, password: hash});
-      user.save().then(result =>{ console.log('registration was successful')})
-      .catch(err=> console.log('There was an error'))
-    });
-    res.redirect('/registration/login')
-  };
+  const errors = req.validationErrors();    
+  const {email, name, password} = req.body;
+
+  User.findOne({email}, (err,emailFound)=>{
+    if(err){console.log('There was an error')}
+    if(emailFound && errors){
+      req.session.errors = [errors[0].msg,'Email is already taken'];
+      res.redirect('/registration/signup');
+    }else if(!emailFound && errors){
+      req.session.errors = [errors[0].msg];
+      res.redirect('/registration/signup');
+    }else{
+      Bcrypt.hash(password, 10,(err, hash)=>{
+        if(err){ console.log('There was an error')}
+          const user = new User({_id: mongoose.Types.ObjectId(), name, email, password: hash});
+          user.save().then(result =>{ console.log('registration was successful')})
+          .catch(err=> console.log('There was an error'));
+      });
+      res.redirect('/registration/login')
+    }
+  });
 };
 
 exports.login = (req, res)=>{
