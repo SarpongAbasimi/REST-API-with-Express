d3f04a1ff576e57bf4fa9cd7370f6dd017ea2ef0
sarpong <abasimisarpong@rocketmail.com>
2019-05-15 11:37:40 +0100

    Make registration code better by using substiution

--- controllers/registration.js
+++ controllers/registration.js
@@ -7,35 +7,36 @@ exports.registration = (req, res)=> {
   res.render('signup',{errors:errors})
 };
 
-exports.submitRegistration = (req, res)=> {
-  req.check('email','The email you entered is invalid').isEmail();
-  req.check('password','Password must be at least 6 characters long').isLength({ min: 6});
-  req.check('password','Password must be equal to your confirmation password').equals(req.body.confirm_password);
+//Make this code better
+// exports.submitRegistration = (req, res)=> {
+//   req.check('email','The email you entered is invalid').isEmail();
+//   req.check('password','Password must be at least 6 characters long').isLength({ min: 6});
+//   req.check('password','Password must be equal to your confirmation password').equals(req.body.confirm_password);
 
-  const errors = req.validationErrors();    
-  const {email, name, password} = req.body;
+//   const errors = req.validationErrors();    
+//   const {email, name, password} = req.body;
 
-  let holder =[]
-  for(let i = 0; i < errors.length; i++){holder.push((errors[i].msg))}
-  User.findOne({email}, (err,emailFound)=>{
-    if(err){console.log('There was an error')}
-    if(emailFound && errors){
-      req.session.errors = [...holder,'Email is already taken'];
-      res.redirect('/registration/signup');
-    }else if(!emailFound && errors){
-      req.session.errors = [...holder];
-      res.redirect('/registration/signup');
-    }else{
-      Bcrypt.hash(password, 10,(err, hash)=>{
-        if(err){ console.log('There was an error')}
-          const user = new User({_id: mongoose.Types.ObjectId(), name, email, password: hash});
-          user.save().then(result =>{ console.log('registration was successful')})
-          .catch(err=> console.log('There was an error'));
-      });
-      res.redirect('/registration/login')
-    }
-  });
-};
+//   let holder =[]
+//   for(let i = 0; i < errors.length; i++){holder.push((errors[i].msg))}
+//   User.findOne({email}, (err,emailFound)=>{
+//     if(err){console.log('There was an error')}
+//     if(emailFound && errors){
+//       req.session.errors = [...holder,'Email is already taken'];
+//       res.redirect('/registration/signup');
+//     }else if(!emailFound && errors){
+//       req.session.errors = [...holder];
+//       res.redirect('/registration/signup');
+//     }else{
+//       Bcrypt.hash(password, 10,(err, hash)=>{
+//         if(err){ console.log('There was an error')}
+//           const user = new User({_id: mongoose.Types.ObjectId(), name, email, password: hash});
+//           user.save().then(result =>{ console.log('registration was successful')})
+//           .catch(err=> console.log('There was an error'));
+//       });
+//       res.redirect('/registration/login')
+//     }
+//   });
+// };
 
 exports.login = (req, res)=>{
   res.render('login')
